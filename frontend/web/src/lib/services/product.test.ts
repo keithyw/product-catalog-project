9b88478cc061f45b7540942bc38eab42d4899f6a
Keith Watanabe <somefakeemail@bleh.com>
2025-07-30 14:25:55 -0700

    fixed product unit test

--- frontend/web/src/lib/services/product.test.ts
+++ frontend/web/src/lib/services/product.test.ts
@@ -31,18 +31,19 @@ describe('productService', () => {
 			}
 
 			mock.onGet(API_PRODUCT_URL).reply(200, mockResponse)
-			const response = await productService.getProducts()
+			const response = await productService.fetch(0, 10)
 			expect(response).toEqual(mockResponse)
 			expect(mock.history.get.length).toBe(1)
 			expect(mock.history.get[0].url).toBe('/products/')
-			expect(mock.history.get[0].params).toBeUndefined()
+			expect(mock.history.get[0].params).toStrictEqual({
+				page: 0,
+				page_size: 10,
+			})
 		})
 
 		it('should handle errors gracefully', async () => {
 			mock.onGet(API_PRODUCT_URL).reply(500)
-			await expect(productService.getProducts()).rejects.toThrow(
-				axios.AxiosError,
-			)
+			await expect(productService.fetch()).rejects.toThrow(axios.AxiosError)
 			expect(mock.history.get.length).toBe(1)
 			expect(consoleErrorSpy).toHaveBeenCalled()
 		})
